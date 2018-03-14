01667fb8776892c6ed5da5ac3ad50e681617f532
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-14 11:22:25 -0400

    Updated from Brython Server: 3/14/2018 11:22:25 AM

--- escapeVelocity.py
+++ escapeVelocity.py
@@ -16,7 +16,7 @@ G=6.674E-11
 Ve=sqrt(2*Me*G/Re)
 print("Predicted escape velocity is", Ve, "m/s")
 #Add a slider to control timezoom
-tz=Slider((10,400),0,5,0,positioning="pyhsical")
+tz=Slider((10,400),0,5,0,positioning="physical")
 
 rocket=Rocket(earth, heading=radians(90), directiond=90, velocity=Ve, timezoom=tz)
 earth.run(rocket)
