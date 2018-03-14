3c820b40d087daf6a40d022e608bcd880a5adde1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-14 10:56:51 -0400

    Updated from Brython Server: 3/14/2018 10:56:51 AM

--- escapeVelocity.py
+++ escapeVelocity.py
@@ -1,1 +1,10 @@
+#James Roth
+#3/14/18
+#escapeVelocity.py - escape velocity for planets
 
+from ggrockets import Rockets, Planet
+from math import radians
+
+earth=Planet
+rocket=Rocket(earth, heading=radians(90), directiond=90, velocity=10)
+earth.run(rocket)
