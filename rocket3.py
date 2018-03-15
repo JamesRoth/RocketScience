5d43f3078f04bcdc13eadbfc219273a304dc1aa9
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-15 08:38:00 -0400

    Updated from Brython Server: 3/15/2018 8:37:59 AM

--- rocket3.py
+++ rocket3.py
@@ -1,1 +1,42 @@
+#James Roth
+#3/15/18
+#rocket3.py - controlling a rocket
 
+from ggrocket import Rocket, Planet
+from math import sqrt, log, radians
+from ggmath import InputButton, Timer
+
+earth=Planet(planetmass=0)
+
+RocketStarted=0
+StartTime=None
+BurnTime=0
+
+me=25600
+mp=395700
+F1D=716000
+N1D=9
+
+Ftotal=F1D*N1D
+tbrun=180
+
+vmax=Ftotal*tburn/(me+mp)
+print("Predicted final velocity (Newton's 2nd law), vmax:", vmax, "m/s")
+
+def GetThrust():
+    global BurnTime
+    global RocketStarted
+    if RocketStarted:
+        BurnTime=rocket.shiptime - StartTime
+        if BurnTime>=tburn:
+            RocketStarted=0
+            return 0
+        else:
+            return Ftotal
+def StartRocket():
+    global RocketStarted
+    global StartTime
+    if not RocketStarted:
+        RocketStarted=1
+        StartTime=rocket,shiptime
+start=InputButton
